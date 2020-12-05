ea47b6d54c4918c099deea1f101421b853382c39
Whitney Naquin <whitneynaquin@MacBook-Pro.attlocal.net>
2020-12-05 07:21:50 -0600

    initial xcode project creation

---
+++ youtube-onedaybuild/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Whitney Naquin on 12/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
